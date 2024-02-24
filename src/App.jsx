91fae27e29e8a1f50de8d53c83232286d0c15c83
Rengkat <successinmotion11@gmail.com>
2024-02-24 22:52:25 +0100

    pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import signUp from "./Pages/signUp";
+import Login from "./Pages/login";
+import SharedRoute from "./Components/SharedRoute";
+import Location from "./Pages/location/Location";
+import History from "./Pages/history/History";
+import Setting from "./Pages/setting/Setting";
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route element={<signUp />} />
+      <Route element={<Login />} />
+      <Route element={<SharedRoute />}>
+        <Route index element={<Location />} />
+        <Route element={<History />} />
+        <Route element={<Setting />} />
+      </Route>
+    </Routes>
   );
 }
 
