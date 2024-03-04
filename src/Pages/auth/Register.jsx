4b91ded81ea99c6ebe911960f9969e8c75233f6c
collinswod <wodopeyc@gmail.com>
2024-03-04 16:51:40 +0000

    Register form updated

--- src/Pages/auth/Register.jsx
+++ src/Pages/auth/Register.jsx
@@ -2,56 +2,34 @@ import React, { useState } from 'react';
 import './Register.css'; // import CSS file for styling
 
 const Register = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const handleRegister = () => {
-    // registration logic 
-    console.log('Registration details:', { firstName, lastName, email, password, rememberMe });
-  };
-
   return (
     <div className="register-container">
-      <h2>Sign Up</h2>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <label>
-        <input
-          type="checkbox"
-          checked={rememberMe}
-          onChange={(e) => setRememberMe(e.target.checked)}
-        />
-        Remember me
-      </label>
-      <button className="register-button" onClick={handleRegister}>Register</button>
+      <form className="signup-form">
+      <h1 className="register-header">Sign Up</h1>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" name="firstName" placeholder="Enter your first name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" name="lastName" placeholder="Enter your last name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email address" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" />
+        </div>
+        <div className='rememberme'>
+        <label htmlFor='checkbox'>Remember me</label>
+        <input type="checkbox" id='checkbox'></input>
+        </div>
+        <button type="submit" className="register-button">Register</button>
+      </form>
     </div>
   );
 };
 
 export default Register;
-
