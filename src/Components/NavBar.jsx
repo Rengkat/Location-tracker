638e2b7f2eb245957138766f0e36258f7c144489
Rengkat <successinmotion11@gmail.com>
2024-03-10 09:43:10 +0100

    adjust nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,7 @@
-
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import profile from "./Image/profile.png";
+// import profile from "./Image/profile.png";
+import { FaUserLarge } from "react-icons/fa6";
 // import { FaUser } from "react-icons/fa";
 import { FaCaretDown } from "react-icons/fa";
 import "./Navbar.css";
@@ -10,32 +10,30 @@ const NavBar = () => {
   return (
     <>
       <nav className="navbar">
-        <h3>
-          <span className="zidio">Zidio</span>
-        </h3>
-        <div className="scr">
-          <input className="search" placeholder="search..." type="text" />
-          <span className="btn"> Search</span>
+        <div className="left">
+          <h3>
+            <span className="zidio">Zidio</span>
+          </h3>
+          <div className="scr">
+            <input className="search" placeholder="search location" type="text" />
+            <span className="btn"> Search</span>
+          </div>
         </div>
-        <div className="notification">
-          <span className="not">Notifications</span>
-          <span className="not-btn">4</span>
+        <div className="right">
+          <div className="notification">
+            <span className="not">Notifications</span>
+            <span className="not-btn">4</span>
+          </div>
+          <div className="user">
+            <span style={{ color: "white" }}>
+              <FaUserLarge fontsize={30} />
+            </span>
+            <span className="use">User</span>
+            <span className="caret">
+              <FaCaretDown />
+            </span>
+          </div>
         </div>
-        <div className="user">
-          <img src={profile}  className="pro"/>
-          <span className="use">User</span>
-          <span className="caret"><FaCaretDown/></span>
-          {/* <span className="not-btn">4</span> */}
-        </div>
-        {/* <div>
-          <FaUser /> User
-        </div> */}
-        {/* <div className="bell">
-          <span className="bell" style={{width:"5rem"}}><FaBell/></span>
-        </div> */}
-        {/* <NavLink>
-        <li>Home</li>
-      </NavLink> */}
       </nav>
     </>
   );
