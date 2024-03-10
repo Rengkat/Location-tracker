e5bf1223db572f3bbf1ccfbef97123b49e06b254
Rengkat <successinmotion11@gmail.com>
2024-03-10 10:10:56 +0100

    settings

--- src/Pages/setting/Setting.jsx
+++ src/Pages/setting/Setting.jsx
@@ -1,5 +1,24 @@
+import "./style.css";
 const Setting = () => {
-  return <div>Setting</div>;
+  return (
+    <div>
+      <div className="setting-container">
+        <div className="language">
+          <h1>Display Language</h1>
+          <p>Manage the language of your Search display, and select preferences for your results</p>
+          <input type="text" />
+        </div>
+        <div className="region">
+          <h1>Region</h1>
+          <p>
+            Select your preferred region for search results. Use controls on the search results page
+            to turn this filter on.
+          </p>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Setting;
